5ffd2b114e277b7affc8441b328b52ad429aeb31
James Benjamin <james.abenj@gmail.com>
2020-05-27 18:08:37 +0000

    Automatically backed up by Learn

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -32,9 +32,6 @@ RSpec.describe PostsController, type: :controller do
     skip("Add a hash of attributes valid for your model")
   }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
